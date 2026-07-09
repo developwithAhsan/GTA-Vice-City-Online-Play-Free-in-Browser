7d64da2eaf9e7a9a4be62e4a7adc2b6c79553a11
Muhammad Ahsan <ahsan.ahsan.2977@gmail.com>
2026-07-09 13:16:48 +0500

    Change Cross-Origin-Embedder-Policy to credentialless

--- vite.config.js
+++ vite.config.js
@@ -77,7 +77,7 @@ export default defineConfig({
     allowedHosts: true,
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Embedder-Policy": "credentialless",
     },
   },
 });

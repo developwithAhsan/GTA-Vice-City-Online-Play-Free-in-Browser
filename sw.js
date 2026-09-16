d6293df2d1e074bf74c0a5f054e839b0505c1a6b
Muhammad Ahsan <ahsan.ahsan.2977@gmail.com>
2026-09-16 21:02:32 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11440782
+    "domain": "3nbf4.com",
+    "zoneId": 11817542
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
